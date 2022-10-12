0e9a9e86902cc5616c5f94935d10489b6c85edf6
rindravictorien <rindradev1112@gmail.com>
2022-10-12 11:41:00 +0300

    remove confirmation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         $validated = request()->validate([
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users,email',
-            'password' => 'required|min:6',
+            'password' => 'required|min:6|confirmed',
         ]);
 
         // create user
